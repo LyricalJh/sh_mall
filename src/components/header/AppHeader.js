d45bd810f2baf61381f2c783ec76ba377d21eb15
소중한 <sojunghan49@gmail.com>
2024-07-23 23:56:23 +0900

    [style] AppHeader fixed

--- src/components/header/AppHeader.js
+++ src/components/header/AppHeader.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
@@ -6,26 +7,31 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 //componemt
 import SearchInput from "../input/SearchInput";
 const AppHeader = () => {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <>
-      <div
-        className="flex items-center justify-between h-20  px-5 border-b-2"
-        style={{ borderColor: "#262626", background: "#000000" }}
-      >
-        <h1 className="font-[GowunBatang] text-[#F5F5F5] text-2xl whitespace-nowrap">
-          오늘의 시톡
-        </h1>
-        <div className="flex items-center">
-          <SearchInput />
-          <span className="ms-4">
-            <FontAwesomeIcon
-              className="text-[#F5F5F5]"
-              size={"lg"}
-              icon={faHeart}
-            />
-          </span>
+      {isHome && (
+        <div
+          className="fixed w-full flex items-center justify-between h-20  px-5 border-b-2 z-50"
+          style={{ borderColor: "#262626", background: "#000000" }}
+        >
+          <h1 className="font-[GowunBatang] text-[#F5F5F5] text-2xl whitespace-nowrap">
+            오늘의 시톡
+          </h1>
+          <div className="flex items-center">
+            <SearchInput />
+            <span className="ms-4">
+              <FontAwesomeIcon
+                className="text-[#F5F5F5]"
+                size={"lg"}
+                icon={faHeart}
+              />
+            </span>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
